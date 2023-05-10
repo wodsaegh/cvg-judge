cc13c9cecfc84043bd3d13e9c00bb5c9a77f0dda
wodsaegh <Wout.DeSaegher@Ugent.be>
2023-05-11 00:36:55 +0200

    removed bug of too big nodes

--- AGCVG.py
+++ AGCVG.py
@@ -3,9 +3,10 @@ import archinfo
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
+import subprocess
+import sys
 
-
-#make a dict of hexadecimal and decimal adresses
+# make a dict of hexadecimal and decimal adresses
 # def gen_dicts(cfg):
 #     func_hexwb = {}
 #     func_decwb = {}
@@ -19,7 +20,7 @@ import json
 #     return func_hexwb, func_decwb
 
 
-#make a list of all adresses
+# make a list of all adresses
 # def gen_addr_list(cfg):
 #     addr_list = []
 #     for node in cfg.nodes():
@@ -34,9 +35,9 @@ def gen_instr_list(cfg):
         # Iterate over all instructions in the block
         for insn in block.capstone.insns:
             instr_dict[insn.address] = f"{insn.mnemonic} {insn.op_str}"
-    sorted_instr_list =  sorted(instr_dict.items(), key=lambda x:x[0])
+    sorted_instr_list = sorted(instr_dict.items(), key=lambda x: x[0])
     return sorted_instr_list
-#get the raw text from the node
+# get the raw text from the node
 # def get_node_text(node,func_hexwb,func_decwb,addr_list):
 #     nodestr = ""
 #     for insn in node.block.capstone.insns:
@@ -50,8 +51,10 @@ def gen_instr_list(cfg):
 #             nodestr += (f"{insn.mnemonic} {insn.op_str} ")     #string of mnemonic and operands
 #     return nodestr
 
-#function that decides wether the arrow is full or dashed
-def decide_jump(src,dest,instr_list):
+# function that decides wether the arrow is full or dashed
+
+
+def decide_jump(src, dest, instr_list):
     src_ins = src.block.capstone.insns[-1]
     dest_ins = dest.block.capstone.insns[0]
     index_src = [el[0] for el in instr_list].index(src_ins.address)
@@ -60,54 +63,93 @@ def decide_jump(src,dest,instr_list):
         return True
     return False
 
-def get_insns(node,instr_list):
+
+def get_insns(node, instr_list):
     nodeinsns = ""
     for insn in node.block.capstone.insns:
-        nodeinsns+= str([el[0] for el in instr_list].index(insn.address)) +","
+        nodeinsns += str([el[0]
+                         for el in instr_list].index(insn.address)) + ","
 
     return nodeinsns[:-1]
 
-#create the nodes that will be in the nodes json
-def create_nodes(cfg,instr_list):
+# create the nodes that will be in the nodes json
+
+
+def create_nodes(cfg, instr_list):
     nodelist = []
+    extra_edges = []
+    changed_nodes = {}
     for node in cfg.nodes():
-        nodeinsns = get_insns(node,instr_list)
+        nodeinsns = get_insns(node, instr_list)
+        inslist = nodeinsns.split(",")
+        for i in range(len(inslist)):  # needed for nodes that do not end in return
+            if (",".join(inslist[i:]) in nodelist):
+                nodeinsns = ",".join(inslist[:i])
+                extra_edges.append(
+                    {"dashes": True, "from": nodeinsns, "to": ",".join(inslist[i:])})
+                changed_nodes[get_insns(node, instr_list)
+                              ] = ",".join(inslist[:i])
         nodelist.append(nodeinsns)
-    return nodelist
+    return nodelist, extra_edges, changed_nodes
 
 
-#create the edges that will be in the edges json
-def create_edges(cfg,instr_list):
-    edgedict = {"Ijk_Ret" : False , "Ijk_Call": False, "Ijk_FakeRet" : True, 'Ijk_Boring' : "decide"}
+# create the edges that will be in the edges json
+def create_edges(cfg, instr_list, changed_nodes):
+    edgedict = {"Ijk_Ret": False, "Ijk_Call": False,
+                "Ijk_FakeRet": True, 'Ijk_Boring': "decide"}
     edges = []
+    fromlist = []
+    tolist = []
     for src, dst, data in cfg.graph.edges(data=True):
         edge = {}
         edge_kind = data["jumpkind"]
-        edge["dashes"]= edgedict[edge_kind] 
-        if(edge["dashes"] == "decide"):
-            edge["dashes"]= decide_jump(src,dst,instr_list)
-        edge["from"]= get_insns(src,instr_list)
-        edge["to"]= get_insns(dst,instr_list)
-        json_edge = json.dumps(edge,indent = 3)
-        edges.append(edge)
+        edge["dashes"] = edgedict[edge_kind]
+        if (edge["dashes"] == "decide"):
+            edge["dashes"] = decide_jump(src, dst, instr_list)
+        edge["from"] = get_insns(src, instr_list)
+        edge["to"] = get_insns(dst, instr_list)
+        if (edge["to"] in changed_nodes.keys()):
+            edge["to"] = changed_nodes[edge["to"]]
+        json_edge = json.dumps(edge, indent=3)
+        if (edge["from"] not in changed_nodes.keys()):
+            edges.append(edge)
     return edges
 
 
 if __name__ == "__main__":
-    p = angr.Project("test", auto_load_libs=False)
+    file = sys.argv[1]
+    with open(file, "r") as file:
+        solution_content = file.read()
+    writefile = "writefile.s"
+    try:
+        if sys.argv[2].lower() == "intel":
+            with open(writefile, "w") as file2:
+                file2.write(".intel_syntax noprefix\n" +
+                            solution_content + "\n")
+        elif sys.argv[2].lower() == "att":
+            with open(writefile, "w") as file2:
+                file2.write(solution_content)
+        else:
+            print("ERROR:Wrong architecture\nCorrect architectures: intel, ATT (=AT&T)")
+        proc = subprocess.run(f"as {writefile} -o runcode", shell=True)
+
+    except:
+        print("ERROR:No architecture given.\nCorrect usage: python3 generateCVG.py <inputfile> <architecture> ")
+
+    p = angr.Project("runcode", auto_load_libs=False)
     # Perform full program analysis
     cfg = p.analyses.CFGFast()
-    # func_hexwb, func_decwb = gen_dicts(cfg) 
+    # func_hexwb, func_decwb = gen_dicts(cfg)
     # addr_list = gen_addr_list(cfg)
     instr_list = gen_instr_list(cfg)
-    nodelist = create_nodes(cfg,instr_list)
-
-    edgeslist = create_edges(cfg,instr_list)
+    nodelist, extra_edges, changed_nodes = create_nodes(cfg, instr_list)
+    edgeslist = create_edges(cfg, instr_list, changed_nodes)
+    for edge in extra_edges:
+        edgeslist.append(edge)
     graph_dict = {
-    'nodes': nodelist,
-    'edges': edgeslist
+        'nodes': nodelist,
+        'edges': edgeslist
     }
-    graph_json = json.dumps(graph_dict,indent = 2)
-    with open("graph.json", "w") as outfile:
+    graph_json = json.dumps(graph_dict, indent=2)
+    with open("solution.json", "w") as outfile:
         outfile.write(graph_json)
-
